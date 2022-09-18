af1c69ca77c69e12744604d93e55dcb3c0ed2541
S-VIN <7530023@gmail.com>
2022-09-18 17:31:56 +0300

    end setup_db script

--- setup_db.py
+++ setup_db.py
@@ -1,6 +1,7 @@
 import psycopg2
 import glob
 import os
+import itertools
 
 connection = psycopg2.connect(
     database="telegram_my_messages", user='postgres', password='3228', host='127.0.0.1', port='5432'
@@ -9,11 +10,46 @@ connection = psycopg2.connect(
 connection.autocommit = True
 cursor = connection.cursor()
 
+def get_last_used_script():
+    cursor.execute('SELECT script_name FROM last_used_script')
+    message_records = cursor.fetchall()
+    return message_records[0][0]
 
-for filename in glob.glob('sql/*.sql'):
+
+def get_number_of_script(script_name):
+    return int(script_name[4:script_name.find('-')])
+
+
+def sql_sort(dict_of_files):
+    files_with_numbers = dict()
+    for filename in dict_of_files:
+        files_with_numbers[get_number_of_script(filename)] = filename
+    return sorted(files_with_numbers.items())
+
+
+def set_executed(filename):
+    cursor.execute('UPDATE last_used_script SET script_name = \'' + filename + '\' WHERE onerow_id=true;')
+
+
+def execute_script(filename):
     with open(os.path.join(os.getcwd(), filename), 'r') as f:
         text = f.name
-        print("starting execution: ", f.name)
         command = open(f.name, "r").read()
-        print(command)
         cursor.execute(command)
+
+sorted_scripts = sql_sort(glob.glob('sql/*.sql'))
+last_script = get_number_of_script(get_last_used_script())
+
+for script_pair in sorted_scripts:
+    if script_pair[0] > last_script:
+        print(script_pair[1], 'EXECUTE')
+        try:
+            execute_script(script_pair[1])
+            set_executed(script_pair[1])
+        except Exception as err:
+            print(script_pair[1], 'ERROR EXECUTION')
+            print(err)
+            print('SCRIPT STOPPED')
+            break
+    else:
+        print(script_pair[1], 'SKIPPED')
--- sql/0-creation.sql
+++ sql/0-creation.sql
@@ -1,24 +1,35 @@
--- CREATE DATABASE telegram_my_messages;
+DO $$
+BEGIN
+    -- CREATE DATABASE telegram_my_messages;
 
-CREATE TABLE users (
-  id BIGINT PRIMARY KEY,
-  first_name TEXT,
-  second_name TEXT,
-  user_name TEXT,
-  is_bot BOOL DEFAULT FALSE,
-  is_in_contacts BOOL DEFAULT TRUE
-);
+    CREATE TABLE IF NOT EXISTS users (
+      id BIGINT PRIMARY KEY,
+      first_name TEXT,
+      second_name TEXT,
+      user_name TEXT,
+      is_bot BOOL DEFAULT FALSE,
+      is_in_contacts BOOL DEFAULT TRUE
+    );
 
-CREATE TABLE messages (
- id BIGINT PRIMARY KEY,
- text TEXT,
- datetime BIGINT NOT NULL,
- user_id BIGINT REFERENCES users(id),
- chat_type INTEGER NOT NULL,
- media_type INTEGER DEFAULT NULL
-);
+    CREATE TABLE IF NOT EXISTS messages (
+     id BIGINT PRIMARY KEY,
+     text TEXT,
+     datetime BIGINT NOT NULL,
+     user_id BIGINT REFERENCES users(id),
+     chat_type INTEGER NOT NULL,
+     media_type INTEGER DEFAULT NULL
+    );
 
-CREATE TABLE unread_messages (
-    message_id BIGINT REFERENCES messages(id)
-)
+    CREATE TABLE  IF NOT EXISTS unread_messages (
+        message_id BIGINT REFERENCES messages(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS last_used_script (
+        onerow_id bool PRIMARY KEY DEFAULT TRUE,
+        script_name TEXT,
+        CONSTRAINT onerow_uni CHECK (onerow_id)
+    );
+END;
+$$;
+
 
